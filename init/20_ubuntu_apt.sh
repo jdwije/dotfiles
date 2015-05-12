8ff5f403f4e7e9e00a2599e64bb15368f6cfc9a4
Jason Wijegooneratne <jason.wijegooneratne@temando.com>
2015-05-13 08:57:04 +1000

    added gnus config

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -38,3 +38,11 @@ if [[ ! "$(type -P git-extras)" ]]; then
     sudo make install
   )
 fi
+
+# Swap Left ALT with Left SUPER [aka. Windows Key]
+xmodmap -e "keycode 64 = Super_L NoSymbol Super_L"    #this will make Alt_L to act as Super_L
+xmodmap -e "keycode 133 = Alt_L Meta_L Alt_L Meta_L"  #this will make Super_L to act as Alt_L
+
+xmodmap -pke > ~/.Xmodmap
+
+echo 'xmodmap .Xmodmap' > ~/.xinitrc
